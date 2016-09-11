39d2eeb99a3095f117f786868d98bcd88244a304
541814919@qq.com <541814919@qq.com>
2016-09-11 11:58:37 +0800

    数据结构整理

---
+++ ConsoleProject/Interator/Node.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject.Interator
+{
+    //堆栈，先进后出http://blog.jobbole.com/79267/
+    class Node
+    {
+        public Node Item { get; set; }
+        public Node Next { get; set; }
+        private Node first = null;
+        private int number = 0;
+
+        void Push(Node node)
+        {
+            Node oldFirst = first;
+            first = new Node();
+            first.Item = node;
+            first.Next = oldFirst;
+            number++;
+        }
+
+        Node Pop()
+        {
+            Node item = first.Item;
+            first = first.Next;
+            number--;
+            return item;
+        }
+    }
+}
